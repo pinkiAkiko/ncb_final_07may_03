d6ad40a958f630190b3042ea334e8c95716c2cc2
pinkiAkiko <pinki.akiko@gmail.com>
2026-05-07 21:37:17 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="min-h-screen flex flex-col bg-background font-poppins">
-        <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:bg-primary focus:text-primary-foreground focus:px-4 focus:py-2 focus:rounded">Skip to main content</a>
+        <a href="#main-content" className="skip-link">Skip to main content</a>
         <UtilityStrip />
         <MainHeader />
         <main id="main-content" className="flex-1">
